b9aba17b86ce63ec36291f3b8ea82251a0711844
Sebastian Spielhagen <s.spielhagen@me.com>
2023-12-12 09:59:34 +0100

    add header design and nav

--- src/App.tsx
+++ src/App.tsx
@@ -6,10 +6,22 @@ import './App.css';
 function App() {
     return (
         <div className="App">
-            <div className="app-header">
-                <h1>Todo Liste</h1>
-            </div>
-            <TodoList />
+            <header className="app-header">
+                <nav>
+                    <button className="nav-button">Home</button>
+                    <button className="nav-button">Aufgabe erstellen</button>
+                    <button className="nav-button">Offene Aufgaben</button>
+                    <button className="nav-button">Begonnene Aufgaben</button>
+                    <button className="nav-button">Erledigte Aufgaben</button>
+                </nav>
+                <h1 className="app-title">Todo Liste</h1>
+            </header>
+            <main>
+                <TodoList />
+            </main>
+            <footer className="app-footer">
+                <p>© 2022 Mein Unternehmen</p>
+            </footer>
         </div>
     );
 }
